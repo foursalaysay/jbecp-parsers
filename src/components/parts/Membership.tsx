123f6751327483674488e5923b3ead02678cfdc3
John Kyle Salaysay <foursalaysay@gmail.com>
2024-10-24 00:25:24 +0800

    for-first-deployment

--- src/components/parts/Membership.tsx
+++ src/components/parts/Membership.tsx
@@ -8,8 +8,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 
 export function Membership() {
   return (
@@ -25,18 +24,7 @@ export function Membership() {
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+         
         </div>
         <DialogFooter>
           <Button type="submit">Save changes</Button>
